ad1805e31d458eb164d8c5f98b23cdd3213c86d6
OlenikPetros <petrosoloc4@gmail.com>
2025-09-10 19:10:55 +0300

    Delete backend/src/main/java/com/example directory

--- backend/src/main/java/com/example/PublishedDate.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
--- backend/src/main/java/com/example/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-
-public interface UserRepository extends MongoRepository<User, String> {
-    // Basic CRUD methods are inherited
-}
